a5af3d4b877ac97dd286433e2639a70bb159222b
Joel Bidal <joelbidal1@hotmail.com>
2025-07-03 17:58:35 -0300

    Website WIP

--- js/app.js
+++ js/app.js
@@ -58,6 +58,7 @@ window.addEventListener('load', function () {
     loadLanguage(lang);
   });
 
+  // Date Picker initializer 
   $(function(){
     $("#datepicker-in").datepicker({
       dateFormat: "dd-mm-yy"
@@ -69,4 +70,39 @@ window.addEventListener('load', function () {
     });
   });
 
+  // Form
+  if ($('#reserve-form').length) {
+    $(function () {
+      const scriptURL =
+        'https://script.google.com/macros/s/AKfycbw7D0tksKhH_dxxAN-DubJlPcJIlCccTE2p4Bo-jdKSdXXRCbpduw-kwWFllm0QNNw/exec';
+      const form = document.getElementById('reserve-form');
+  
+      form.addEventListener('submit', (e) => {
+        $('#reserve-form').addClass('disabled');
+  
+        // Sending status
+        $('#reserve-form').addClass('readonly');
+        $('#reserve-form input.button').val("Sending Information");
+  
+        e.preventDefault();
+  
+        // Crear FormData para incluir archivo y otros campos
+        const formData = new FormData(form);
+  
+        fetch(scriptURL, { method: 'POST', body: formData })
+          .then((response) => {
+            $('#success-message').fadeIn();
+            $('#reserve-form').addClass('readonly');
+            $('#reserve-form input.button').val("Submitted");
+          })
+          .catch((error) => {
+            console.error('Error!', error.message);
+            $('#success-message').fadeOut();
+            $('#reserve-form').removeClass('readonly');
+            $('#reserve-form input.button').val("Submit Information");
+          });
+      });
+    });
+  }
 });
+
